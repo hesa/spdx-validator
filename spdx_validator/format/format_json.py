3e68174a097e4dfc442bbd7b1f0d41872aef17b9
Sandklef Henrik <hesa@sandklef.com>
2021-09-21 20:52:06 +0200

    new format functionality

---
+++ spdx_validator/format/format_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# SPDX-FileCopyrightText: 2020 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import json
+
+from spdx_validator.format.format_interface import FormatInterface
+
+class JsonFormatter(FormatInterface):
+
+    def __init__(self):
+        pass
+    
+    def format_packages(self, packages):
+        return json.dumps(packages)
+
+    def convert(self, data):
+        return json.dumps(data)
+    
