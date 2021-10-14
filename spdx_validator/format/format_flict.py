4ebf4b61edc6aeeac8ebd0d569500cadb4ce7c67
Sandklef Henrik <hesa@sandklef.com>
2021-10-14 20:05:22 +0200

    use name instead of SPDXID, implement flict output of packages

--- spdx_validator/format/format_flict.py
+++ spdx_validator/format/format_flict.py
@@ -15,7 +15,7 @@ class FlictFormatter(FormatInterface):
 
     def _package_to_flict(self, package):
         return {
-            "name": package['SPDXID'],
+            "name": package['name'],
             "license": package['licenseConcluded'],
             "dependencies": []
         }
@@ -23,25 +23,39 @@ class FlictFormatter(FormatInterface):
     def _package_to_flict_project(self, package, deps):
         return {
             "project": {
-                "name": package['SPDXID'],
+                "name": package['name'],
                 "license": package['licenseConcluded'],
                 "dependencies": deps
             }
         }
         
     
-    def format_packages(self, packages):
+    def format_packages(self, package, packages, package_name = None):
         package_list = []
-        for p in packages:
+        for _p in packages:
+            p = _p['package']
+            dependencies = _p['dependencies']
+            if package_name != None and package_name != p['name']:
+                continue
             deps = []
-            for dep in p['dependencies']:
-                dep_map = self._package_to_flict(dep)
-                deps.append(dep_map)
-            top = self._package_to_flict_project(p['package'], deps)
+            for d in dependencies:
+                deps.append(self._package_to_flict(d))
+            top = self._package_to_flict_project(p, deps)
             package_list.append(top)
-        return json.dumps(package_list, indent=4)
+
+        if package_name != None:
+            if len(package_list) == 1:
+                return json.dumps(package_list[0], indent=4)
+            else:
+                return json.dumps(package_list, indent=4)
+        else:
+            return json.dumps(package_list, indent=4)
     
-                            
+    def _format_package(self, package, dependencies):
+        print("package: " + str(package['name']))
+        print("dependencies: " + str(dependencies))
+        for dep in dependencies:
+            print(" ----- : " + str(dep))
     
     def convert(self, data):
-        return "no implementation for flict"
+        return "no implementation for flict" 
